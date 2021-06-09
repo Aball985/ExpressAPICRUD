003e03bf9bd0d7c169e8b77765f613e28ccbdb2f
Aball985 <arball44@gmail.com>
2021-06-09 06:21:54 -0400

    Updated folder structure and worked on backend

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,6 +1,6 @@
 const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
@@ -18,7 +18,6 @@ const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 
 module.exports = db;
